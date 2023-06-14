1c330889a989a4b087e92b3603382de28e948a41
JUAN GERARDO TOVAR RAMIREZ <LRS17110289@purisima.tecnm.mx>
2023-06-14 15:59:14 -0600

    commit

--- tecniprint/SimiSoft/frmCotizacionesUV.cs
+++ tecniprint/SimiSoft/frmCotizacionesUV.cs
@@ -49,7 +49,7 @@ namespace TP
             SaveFileDialog guardar = new SaveFileDialog();
             guardar.FileName = DateTime.Now.ToString("ddMMyyyyHHmmss")+".pdf";
 
-            string paginahtml_texto = Properties.Resources.plantilla.ToString();
+            string paginahtml_texto = "<table><tr><td></td>HOLA</tr></table>";
 
             if (guardar.ShowDialog() == DialogResult.OK)
             {
@@ -66,7 +66,7 @@ namespace TP
 
                     using (StreamReader sr = new StreamReader(paginahtml_texto))
                     {
-                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                        XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr)
                     }
 
                     pdfDoc.Close();
